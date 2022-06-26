3ea0f0b386befbe5e41997e346df9918f504da35
rkriar <ramneekriar@gmail.com>
2022-06-26 15:56:36 -0400

    Added second solution to longestConsecutive in Arrays and Hashing

--- Arrays_and_Hashing/longestConsecutiveSequence.py
+++ Arrays_and_Hashing/longestConsecutiveSequence.py
@@ -11,3 +11,23 @@ class Solution:
                     length += 1
                 longest = max(length, longest)
         return longest
+    
+    def longestConsecutive_2(nums: list[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        
+        nums_sorted = sorted(nums)
+        max_length = 1
+        length = 1
+        
+        for i in range(1, len(nums_sorted)):
+            if nums_sorted[i] == nums_sorted[i-1]:
+                continue
+            elif nums_sorted[i] == (nums_sorted[i-1] + 1):
+                length += 1
+            else:
+                max_length = max(max_length, length)
+                length = 1
+        
+        max_length = max(max_length, length)
+        return max_length
