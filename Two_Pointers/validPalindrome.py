7b2cfdf863b2c02ca1c0e3bb78e25cdce1456080
rkriar <ramneekriar@gmail.com>
2022-07-09 23:53:07 -0400

    Added second solution to isPalindrome

--- Two_Pointers/validPalindrome.py
+++ Two_Pointers/validPalindrome.py
@@ -16,3 +16,21 @@ class Solution:
         return (ord('A') <= ord(c) <= ord('Z') or
                 ord('a') <= ord(c) <= ord('z') or
                 ord('0') <= ord(c) <= ord('9'))
+
+    def isPalindrome_2(self, s:str) -> bool:
+        left = 0
+        right = len(s) - 1
+        
+        while left<right:
+            if s[left].lower()==s[right].lower():
+                left+=1
+                right-=1
+                continue
+            
+            elif not (65<=ord(s[left])<=90 or 97<=ord(s[left])<=122 or 48<= ord(s[left])<=57):
+                left+=1
+            elif  not (65<=ord(s[right])<=90 or 97<=ord(s[right])<=122 or 48<=ord(s[right])<=57):
+                right-=1
+            else:
+                return False
+        return True
