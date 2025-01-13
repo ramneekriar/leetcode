ab873b06e64533e3f853d440677f4b423f1d30ad
ramneekriar <ramneek.riar@torontomu.ca>
2025-01-13 16:17:20 -0500

    0509-fibonacci-sequence add time and space complexity

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -5,6 +5,9 @@ class Solution:
             return n
         return self.fib(n - 1) + self.fib(n - 2)
 
+# Time = O(2^n) because we are creating 2 branches for each call and n is the height of the tree
+# Space = O(n)
+
     # Memoization
     memo = {0:0, 1:1}
     def fibMemo(self, n: int) -> int:
@@ -12,3 +15,6 @@ class Solution:
             return self.memo[n]
         self.memo[n] = self.fibMemo(n - 1) + self.fibMemo(n - 2)
         return self.memo[n]
+
+# Time = O(n) since we are only doing one calculation for each number that we encounter, only following down one branch
+# Space = O(n)
