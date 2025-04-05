dc461acda60a65dd2fb90d8953d9eacb7a62dcb6
ramneekriar <ramneek.riar@torontomu.ca>
2025-04-05 14:24:34 -0400

    Edited 0049-group-anagrams

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -9,5 +9,22 @@ class Solution:
         
         return list(hashmap.values())
     
-# Time = O(m x n) (m = total number of input strings, n = avg len of string)
+# Time = O(n * k log k) where n is the number of words and k is the average length of a word
 # Space = O(n)
+
+    def groupAnagrams_2(self, strs: list[str]) -> list[list[str]]:
+        hashmap = defaultdict(list)
+
+        for word in strs:
+            word = word.lower()
+            count = [0] * 26
+            for letter in word:
+                count[ord(letter) - ord('a')] += 1
+            
+            key = tuple(count)
+            hashmap[key].append(word)
+        
+        return hashmap.values()
+
+# Time: O(nk * 26) where n is the number of words and k is the number of letters in a word and 26 is constant but it's the letters we create everytime
+# Space: O(nk)
